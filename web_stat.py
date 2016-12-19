136546c5014cad294d86a84b99f61bc0a4eac692
Shobhit Kumar <kumar@shobhit.info>
2016-12-19 22:54:08 +0530

    Calculate the linear model given the data and plot the same
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- web_stat.py
+++ web_stat.py
@@ -14,15 +14,32 @@ y = y[~sp.isnan(y)]
 print("After Cleaning x, y")
 print x.shape, y.shape
 
-plt.scatter(x,y)
-plt.title("Web traffic over the last month")
-plt.xlabel("Time")
-plt.ylabel("HIts/hour")
+def plot_data(x, y):
+    plt.scatter(x,y)
+    plt.title("Web traffic over the last month")
+    plt.xlabel("Time")
+    plt.ylabel("HIts/hour")
 
-plt.xticks([w*7*24 for w in range(5)], ['week %i'%w for w in range(5)])
+    plt.xticks([w*7*24 for w in range(5)], ['week %i'%w for w in range(5)])
 
-plt.autoscale(tight=True)
-plt.grid()
+    plt.autoscale(tight=True)
+    plt.grid()
+
+def error(f, x, y):
+    return sp.sum((f(x) - y)**2)
+
+def plot_model_linear(x, y):
+    # Polyfit with linear model: order=1
+    fp1 = sp.polyfit(x, y, 1)
+    print ("Model parameters: %s" % fp1)
+
+    f1 = sp.poly1d(fp1)
+    print (error(f1, x, y))
+
+    fx = sp.linspace(0, x[-1], 1000)
+    plt.plot(fx, f1(fx), color="red", linewidth=4)
+    plt.legend(["d=%i" % f1.order], loc="upper left")
+
+plot_data(x, y)
+plot_model_linear(x, y)
 plt.show()
-
-
